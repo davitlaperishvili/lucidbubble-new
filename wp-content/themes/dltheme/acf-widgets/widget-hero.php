9107d9ee894c6bac15d59c1046f5d4d5d6e6fab5
dlaperishvili <davitlaperishvili@gmail.com>
2024-12-12 09:53:49 +0400

    helper commit

--- wp-content/themes/dltheme/acf-widgets/widget-hero.php
+++ wp-content/themes/dltheme/acf-widgets/widget-hero.php
@@ -3,6 +3,8 @@
         $hero_title = get_sub_field('hero_title'); 
         $hero_subtitle = get_sub_field('hero_subtitle'); 
         $hero_top_title = get_sub_field('hero_top_title'); 
+        $use_video = get_sub_field('use_video'); 
+        $video_iframe = get_sub_field('video_iframe'); 
         $hero_image = get_sub_field('hero_image'); 
         $hero_buttons = get_sub_field('hero_buttons'); 
         $rowIndex = get_row_index();
@@ -29,9 +31,19 @@
               ?>
             </div>
           </div>
-          <figure>
-              <img src="<?php echo $hero_image['url'] ?>" alt="<?php echo $hero_image['alt'] ?>">
-          </figure>
+          <?php 
+            if($use_video){
+              ?>
+                <div class="video_iframe"><?php echo $video_iframe ?></div>
+              <?php
+            }else{
+              ?>
+                <figure>
+                    <img src="<?php echo $hero_image['url'] ?>" alt="<?php echo $hero_image['alt'] ?>">
+                </figure>
+              <?php
+            }
+          ?>
         </div>
       </div>
     </section>
